8e285f0ff9138a43e94b175193ec6abf12d75b92
mhaaron <s14120066@alumnos.itsur.edu.mx>
2020-02-20 23:35:55 -0600

    auth

--- api/components/auth/index.js
+++ api/components/auth/index.js
@@ -1,5 +1,4 @@
 const store = require('../../../store/dummy')
 const ctrl = require('./controller')
 
-// hacer función a controller e inyectarle el store
 module.exports = ctrl(store)
--- store/dummy.js
+++ store/dummy.js
@@ -14,8 +14,12 @@ async function get(tabla, id) {
 }
 
 async function upsert(tabla, data) {
+    if (!db[tabla]) {
+        db[tabla] = []
+    }
+    
     db[tabla].push(data)
-    return db
+    console.log(db)
 }
 
 async function remove(tabla, id) {
